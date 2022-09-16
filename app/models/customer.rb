0ab613bb8dfae51c114a72d5fb208a0ce22bbe73
Tarcisio Oliveira <tarcisio@ymail.com>
2022-09-16 00:01:49 -0300

    fixtures

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
 class Customer < ApplicationRecord
+  def full_name
+    "Sr. #{name}"
+  end
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,32 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'Create Customer' do
+    subject.name = 'Tarcisio Oliveira'
+    subject.email = 'tarcisio@email.com'
+    subject.save
+
+    expect(subject.name).to eq('Tarcisio Oliveira')
+    expect(subject.full_name).to eq('Sr. Tarcisio Oliveira')
+  end
+end
+
+RSpec.describe Customer, type: :model do
+  fixtures :customers
+
+  it 'Create Customer with fixture Tarcisio' do
+    usuario = customers(:tarcisio)
+
+    expect(usuario.name).to eq('Tarcisio Oliveira')
+    expect(usuario.full_name).to eq('Sr. Tarcisio Oliveira')
+  end
+  
+  it 'Create Customer with fixture Fulano' do
+    usuario = customers(:fulano)
+
+    expect(usuario.name).to eq('Fulano da Silva')
+    expect(usuario.full_name).to eq('Sr. Fulano da Silva')
+  end
 end
