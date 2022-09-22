5ba0bbb635e778234374c2c4cceac5fe8180e053
Tarcisio Oliveira <tarcisio@ymail.com>
2022-09-21 23:47:43 -0300

    teste sublime insert rubocop yml

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,12 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2022_09_15_031314) do
-
   create_table "customers", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.boolean "vip"
+    t.integer "days_to_pay"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
--- spec/factories/customer.rb
+++ spec/factories/customer.rb
@@ -4,5 +4,10 @@ FactoryBot.define do
   factory :customer, aliases: %i[user father] do
     name { Faker::Name.name }
     email { Faker::Internet.email }
+
+    factory :customer_vip do
+      vip { true }
+      days_to_pay { 30 }
+    end
   end
 end
